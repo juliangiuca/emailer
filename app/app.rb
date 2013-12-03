7563f58a5616e8cf3d08f7926aac267c84c1341d
localTOAST <localtoast@eggandjam.com>
2013-12-02 23:20:54 -0800

    add dumb logging

--- app/app.rb
+++ app/app.rb
@@ -12,6 +12,7 @@ module Emailer
     use ActiveRecord::ConnectionAdapters::ConnectionManagement
 
     use Rack::Auth::Basic, "Restricted Area" do |username, password|
+      puts "username is: #{username}"
       username == Settings.basic_auth.username && password == Settings.basic_auth.password
     end
 
