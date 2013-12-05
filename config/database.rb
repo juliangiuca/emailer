8126763a09fc7742dc268945873146e33effc0a3
localTOAST <localtoast@eggandjam.com>
2013-12-04 21:19:03 -0800

    database fiddling

--- config/database.rb
+++ config/database.rb
@@ -3,12 +3,15 @@ require 'active_record'
 require_relative "../lib/settings"
 
 db = Settings.database
-
-ActiveRecord::Base.establish_connection(
+connection = {
   :adapter  => 'postgresql',
   :host     => db.host,
   :username => db.username,
-  :password => db.password,
   :database => db.database,
   :encoding => 'utf8'
-)
+}
+connection.merge!({
+  :password => db.password
+}) if RACK_ENV == "production"
+
+ActiveRecord::Base.establish_connection(connection)
