84336df8ecb85800b140ffaaa8eede6e70d768b3
localTOAST <localtoast@eggandjam.com>
2013-12-04 20:59:09 -0800

    use unicorns restart logic

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -20,13 +20,14 @@ stderr_path "#{@dir}/logs/unicorn.stderr.log"
 stdout_path "#{@dir}/logs/unicorn.stdout.log"
 
 before_fork do |server, worker|
-  old_pid = File.join(@dir, 'tmp', 'pids', 'unicorn.pid.oldbin')
 
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
+   old_pid = "#{server.config[:pid]}.oldbin"
+   if old_pid != server.pid
+     begin
+       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+       Process.kill(sig, File.read(old_pid).to_i)
+     rescue Errno::ENOENT, Errno::ESRCH
+     end
+   end
+
 end
