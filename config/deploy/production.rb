5cbc0ce0c2374e71c1fa6442d8950f1c8ae6795c
localTOAST <localtoast@eggandjam.com>
2014-04-10 08:44:20 -0700

    logs to log

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@ set :stage, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '198.199.103.203', user: 'emailer', roles: %w{app web db}, port: 18500
+server 'innocuous.me', user: 'emailer', roles: %w{app web db}, port: 18500
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
