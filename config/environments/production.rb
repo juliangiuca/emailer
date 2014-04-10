aee37c7bb90c8d9179618c2b352c0d21b17084e7
localTOAST <localtoast@eggandjam.com>
2014-04-09 22:46:07 -0700

    add production thingies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Emailer::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+   config.assets.precompile += %w( emailer.js emailer.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -77,4 +77,16 @@ Emailer::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: 'innocuous.me' }
+  config.asset_host = 'http://innocuous.me'
+  IMAGE_LOCATION = "http://i.innocuous.me/"
+
+  Mail.defaults do
+     delivery_method :smtp, { :address              => "smtp.gmail.com",
+                              :port                 => 587,
+                              :authentication       => 'XOAUTH2',
+                              :enable_starttls_auto => true  }
+ end
+
 end
