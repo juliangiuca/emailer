025626b088bdb06d218d462ec4d0482bf54b0f1c
localTOAST <localtoast@eggandjam.com>
2014-05-01 16:13:40 -0700

    to_s on the date_first_viewed

--- app/serializers/tracking_pixel_serializer.rb
+++ app/serializers/tracking_pixel_serializer.rb
@@ -1,3 +1,7 @@
 class TrackingPixelSerializer < ActiveModel::Serializer
   attributes :id, :email_id, :contact_id, :time_viewed, :sent, :date_first_viewed, :views
+
+  def date_first_viewed
+    object.date_first_viewed.to_s
+  end
 end
