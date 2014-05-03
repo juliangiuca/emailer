50de1d61f856111d89e6eb3e038dfb6acbce05b0
localTOAST <localtoast@eggandjam.com>
2014-05-03 13:19:05 -0700

    split opened, unopened, and unsent

--- app/assets/javascripts/emailer/controllers/metricsCtrl.js
+++ app/assets/javascripts/emailer/controllers/metricsCtrl.js
@@ -1,6 +1,7 @@
 emailer.controller("metricsCtrl", [ "$scope", "$http", "$routeParams", 
            function metricsCtrl ($scope, $http, $routeParams) {
-  $scope.opened   = _.select($scope.recipients, function (r) { return r.tracking_pixel.sent != undefined })
-  $scope.unopened = _.select($scope.recipients, function (r) { return r.tracking_pixel.sent == undefined })
+  $scope.opened   = _.select($scope.recipients, function (r) { return r.tracking_pixel.date_first_viewed != undefined })
+  $scope.unopened = _.select($scope.recipients, function (r) { return r.tracking_pixel.sent == true && r.tracking_pixel.date_first_viewed == undefined })
+  $scope.unsent   = _.select($scope.recipients, function (r) { return r.tracking_pixel.sent == undefined })
 }])
 
