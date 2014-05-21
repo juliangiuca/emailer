6b982139cf23ac1f40f00a07a12406ef4f9f45f7
localTOAST <localtoast@eggandjam.com>
2014-05-21 08:02:42 -0700

    clean up the services for groups

--- app/assets/javascripts/groups/services/email.js
+++
@@ -1,8 +0,0 @@
-groups.factory('Email', ['$resource', function($resource) {
-  return $resource('/api/v1/emails/:emailId',
-    {emailId:'@id'},
-    {
-      deliver: {method: 'POST', url: "/api/v1/emails/:emailId/deliver"},
-    }
-  )
-}])
