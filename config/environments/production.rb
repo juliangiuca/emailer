798d4b67b60e49e74c4be9c90d88850fdce25a4a
Julian Giuca <julian@newrelic.com>
2015-04-06 12:06:34 -0700

    dockerify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Emailer::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
@@ -80,7 +80,7 @@ Emailer::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { host: 'innocuous.me' }
-  config.asset_host = 'http://innocuous.me'
+  #config.asset_host = 'http://innocuous.me'
   IMAGE_LOCATION = "http://i.innocuous.me/tp/"
 
   Mail.defaults do
